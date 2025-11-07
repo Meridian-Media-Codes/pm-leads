e6305dc50a767f9907c5e3fa970bd4675df0f812
Meridian-Media-Codes <sam@meridian-media.co.uk>
2025-11-07 22:26:54 +0000

    Update form-handler.php

--- public/form-handler.php
+++ public/form-handler.php
@@ -2,75 +2,6 @@
 if ( ! defined('ABSPATH') ) exit;
 
 add_action('init', function () {
-    if (! isset($_POST['pm_leads_submit'])) return;
-    if (! isset($_POST['pm_leads_nonce']) || ! wp_verify_nonce($_POST['pm_leads_nonce'], 'pm_leads_submit')) return;
-
-    $current = isset($_POST['current_postcode']) ? sanitize_text_field($_POST['current_postcode']) : '';
-    $new     = isset($_POST['new_postcode']) ? sanitize_text_field($_POST['new_postcode']) : '';
-
-    $beds_n  = isset($_POST['bedrooms_new']) ? absint($_POST['bedrooms_new']) : 0;
-    $beds_c  = isset($_POST['bedrooms_current']) ? absint($_POST['bedrooms_current']) : 0;
-
-    $email   = isset($_POST['customer_email']) ? sanitize_email($_POST['customer_email']) : '';
-    $msg     = isset($_POST['customer_message']) ? sanitize_textarea_field($_POST['customer_message']) : '';
-
-    $name    = isset($_POST['customer_name']) ? sanitize_text_field($_POST['customer_name']) : '';
-    $addr_current = isset($_POST['current_address']) ? sanitize_text_field($_POST['current_address']) : '';
-    $addr_new     = isset($_POST['new_address']) ? sanitize_text_field($_POST['new_address']) : '';
-
-    if (!$current || !$new || !$email) {
-        wp_die(__('Missing required fields', 'pm-leads'));
-    }
-
-    // Create the job
-    $title = sprintf('Move %s to %s', $current, $new);
-    $job_id = wp_insert_post([
-        'post_type'   => 'pm_job',
-        'post_title'  => $title,
-        'post_status' => 'publish',
-    ]);
-    if (is_wp_error($job_id) || ! $job_id) {
-        wp_die(__('Could not create job', 'pm-leads'));
-    }
-
-    update_post_meta($job_id, 'current_postcode', $current);
-    pm_job_geocode($job_id, $current);
-
-    update_post_meta($job_id, 'new_postcode', $new);
-    update_post_meta($job_id, 'bedrooms_new', $beds_n);
-    update_post_meta($job_id, 'bedrooms_current', $beds_c);
-
-    update_post_meta($job_id, 'customer_name', $name);
-    update_post_meta($job_id, 'customer_email', $email);
-    update_post_meta($job_id, 'customer_message', $msg);
-
-    update_post_meta($job_id, 'current_address', $addr_current);
-    update_post_meta($job_id, 'new_address', $addr_new);
-
-    update_post_meta($job_id, 'purchase_count', 0);
-
-    do_action('pm_leads_job_created', $job_id);
-
-    // Assign "available" status
-    $term = term_exists('available', 'pm_job_status');
-    if (! $term) {
-        $term = wp_insert_term('available', 'pm_job_status');
-    }
-    if (! is_wp_error($term) && isset($term['term_id'])) {
-        wp_set_post_terms($job_id, [$term['term_id']], 'pm_job_status', false);
-    }
-
-    // Redirect
-    $redirect = home_url('/thank-you/');
-    wp_safe_redirect($redirect);
-    exit;
-});
-
-
-<?php
-if ( ! defined('ABSPATH') ) exit;
-
-add_action('init', function () {
 
     if (! isset($_POST['pm_leads_submit'])) return;
     if (! isset($_POST['pm_leads_nonce']) || ! wp_verify_nonce($_POST['pm_leads_nonce'], 'pm_leads_submit')) return;
@@ -124,8 +55,8 @@ add_action('init', function () {
     /**
      * ✅ GEO (both origin + destination)
      */
-    pm_job_geocode($job_id, $current, 'pm_job_from');
-    pm_job_geocode($job_id, $new,     'pm_job_to');
+    pm_job_geocode($job_id, $current);
+    pm_job_geocode($job_id, $new);
 
     do_action('pm_leads_job_created', $job_id);
 
@@ -143,6 +74,3 @@ add_action('init', function () {
     wp_safe_redirect($redirect);
     exit;
 });
-
-
-
